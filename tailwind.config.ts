93f8f03b48ef90b69d7c1488172af64bfba39a80
hattaltd <107340549+hattagh73two@users.noreply.github.com>
2023-10-24 00:12:27 +0800

    ADD: File tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "clr-text-primary": "#F1F1F1",
+                "clr-text-secondary": "#AAAAAA",
+                "clr-bg-primary": "#0F0F0F",
+                "clr-bg-secondary": "#272727",
+            },
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+        },
+    },
+    plugins: [],
+};
+export default config;
