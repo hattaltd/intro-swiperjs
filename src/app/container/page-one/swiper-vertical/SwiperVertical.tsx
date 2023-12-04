d8a5d4ab763eb25c05054a99dfb1cdc6a0ee3c70
hattaltd <107340549+hattagh73two@users.noreply.github.com>
2023-12-05 02:59:52 +0800

    UPDATE: Logic on breakpoints

--- src/app/container/page-one/swiper-vertical/SwiperVertical.tsx
+++ src/app/container/page-one/swiper-vertical/SwiperVertical.tsx
@@ -1,74 +1,83 @@
 "use client";
+
+import { iClassName } from "@/app/type";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Mousewheel, Pagination } from "swiper/modules";
+import { Mousewheel, Pagination, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
+import classNames from "classnames";
 
-const SwiperVertical = () => {
-  return (
-    <section className="h-screen">
-      {/* Swiper horizontal */}
-      <div className="pt-6 max-w-[1600px] mx-auto">
-        <div className="border bg-clr-text-secondary pl-4 py-2">
-          <h1 className="font-medium text-lg">Swiper for horizontal</h1>
-        </div>
-        <Swiper
-          className="mt-2 h-[420px]"
-          loop
-          mousewheel={true}
-          pagination={{
-            clickable: true,
-          }}
-          breakpoints={{
-            0: {
-              slidesPerView: 1,
-              spaceBetween: 15,
-            },
-            769: {
-              slidesPerView: 3,
-              spaceBetween: 30,
-            },
-          }}
-          modules={[Mousewheel, Pagination]}
-        >
-          {[1, 2, 3, 4, 5, 6, 7].map((i, index) => (
-            <SwiperSlide key={index}>
-              <Image
-                alt={`Image number ${i}`}
-                src={`https://source.unsplash.com/random?${i}`}
-                fill
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-      {/* Swiper vertical */}
-      <div className="pt-6 max-w-[1600px] mx-auto">
-        <div className="border bg-clr-text-secondary pl-4 py-2">
-          <h1 className="font-medium text-lg">Swiper for vertical</h1>
-        </div>
-        <Swiper
-          className="mt-2 h-[680px] rounded-2xl"
-          direction={"vertical"}
-          mousewheel={true}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[Mousewheel, Pagination]}
-        >
-          {[1, 2, 3, 4, 5, 6, 7].map((i, index) => (
-            <SwiperSlide key={i}>
-              <Image
-                alt={`Image number ${i}`}
-                src={`https://source.unsplash.com/random?${i}`}
-                fill
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-    </section>
-  );
+const SwiperVertical = ({ className }: iClassName) => {
+    className = "font-medium text-lg text-green-700";
+
+    return (
+        <section className="h-screen">
+            {/* Swiper horizontal */}
+            <div className="pt-6 max-w-[1600px] mx-auto">
+                <div className="border bg-clr-text-secondary pl-4 py-2">
+                    <h1 className={className}>Swiper for horizontal</h1>
+                </div>
+                <Swiper
+                    className="mt-2 h-[420px]"
+                    modules={[Mousewheel, Pagination, Navigation]}
+                    loop
+                    mousewheel={true}
+                    pagination={true}
+                    navigation={true}
+                    breakpoints={{
+                        0: {
+                            slidesPerView: 1,
+                            spaceBetween: 15,
+                            pagination: { enabled: false },
+                            navigation: { enabled: false },
+                        },
+                        769: {
+                            slidesPerView: 3,
+                            spaceBetween: 30,
+                            pagination: { enabled: true, clickable: true },
+                            navigation: { enabled: true },
+                        },
+                    }}
+                >
+                    {[1, 2, 3, 4, 5, 6, 7].map((i, index) => (
+                        <SwiperSlide key={index}>
+                            <Image
+                                alt={`Image number ${i}`}
+                                src={`https://source.unsplash.com/random?${i}`}
+                                fill
+                            />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+            {/* Swiper vertical */}
+            <div className="pt-6 max-w-[1600px] mx-auto">
+                <div className="border bg-clr-text-secondary pl-4 py-2">
+                    <h1 className="font-medium text-lg">Swiper for vertical</h1>
+                </div>
+                <Swiper
+                    className="mt-2 h-[680px] rounded-2xl"
+                    direction={"vertical"}
+                    mousewheel={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Mousewheel, Pagination]}
+                >
+                    {[1, 2, 3, 4, 5, 6, 7].map((i, index) => (
+                        <SwiperSlide key={i}>
+                            <Image
+                                alt={`Image number ${i}`}
+                                src={`https://source.unsplash.com/random?${i}`}
+                                fill
+                            />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </section>
+    );
 };
 export default SwiperVertical;
